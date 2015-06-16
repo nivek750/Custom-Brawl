14ef2573838be5181cb7273b565f1796f2bfdf55
Kevin Ha <nivek750@hotmail.com>
2015-06-16 19:21:34 +1000

    Uploaded Project

---
+++ gen/softtoasty/custombrawl/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package softtoasty.custombrawl;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/softtoasty/custombrawl/Rectangle.java
@@ -0,0 +1,20 @@
+package softtoasty.custombrawl;
+
+public class Rectangle {
+	public int xPos;
+	public int yPos;
+	public int width;
+	public int height;
+	
+	Rectangle(int i, int j){
+		xPos = i;
+		yPos = j;
+		width = 32;
+		height = 64;
+	}
+	
+	public void SetPosition(int i, int j){
+		xPos = i;
+		yPos = j;
+	}
+}
